15abe9b24143e5770a3d953461ea637d1620c3d9
Judah <judahlit03@gmail.com>
2025-06-23 01:25:28 +0200

    Added a test page

---
+++ pages/test.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Test Page</h1>
+    <div>
+    <p>This is a test page to verify the functionality of the application.</p>
+    <button @click="testFunction">Click Me!</button>
+    </div>
+</template>
+
+<script setup>
+const runtimeConfig = useRuntimeConfig();
+console.log("Runtime Config:", runtimeConfig.public);
+</script>
